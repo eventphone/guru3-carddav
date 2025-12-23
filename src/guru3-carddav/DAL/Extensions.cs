ea5c4451302a02e1078a979585ba5f18e115bf17
zivillian <devel@zivillian.de>
2025-12-23 16:02:05 +0100

    fix npsql date handling

--- src/guru3-carddav/DAL/Extensions.cs
+++ src/guru3-carddav/DAL/Extensions.cs
@@ -7,7 +7,7 @@ namespace eventphone.guru3.carddav.DAL
     {
         public static IQueryable<Event> Active(this IQueryable<Event> source)
         {
-            var now = DateTime.Now.Date;
+            var now = DateTime.UtcNow.Date;
             return source.Where(x => x.RegistrationStart != null || x.IsPermanentAndPublic)
                 .Where(x => x.RegistrationStart <= now || x.IsPermanentAndPublic)
                 .Where(x => x.End != null || x.IsPermanentAndPublic)
@@ -16,7 +16,7 @@ namespace eventphone.guru3.carddav.DAL
 
         public static IQueryable<Extension> Active(this IQueryable<Extension> source)
         {
-            var now = DateTime.Now.Date;
+            var now = DateTime.UtcNow.Date;
             return source.Where(x => x.Event.RegistrationStart != null || x.Event.IsPermanentAndPublic)
                 .Where(x => x.Event.RegistrationStart <= now || x.Event.IsPermanentAndPublic)
                 .Where(x => x.Event.End != null || x.Event.IsPermanentAndPublic)
